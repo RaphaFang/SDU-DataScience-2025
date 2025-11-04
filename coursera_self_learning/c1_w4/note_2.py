46610914922d4ca8cd20327e0f65e128383358d9
方思渝 <fangsihyu0211@gmail.com>
2025-11-04 12:43:34 +0100

    add: note for final assignment of c1

---
+++ coursera_self_learning/c1_w4/note_2.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):    
+    np.random.seed(1)
+    grads = {}
+    costs = []                              # to keep track of the cost
+    m = X.shape[1]                           # number of examples
+    (n_x, n_h, n_y) = layers_dims
+    
+    parameters = {
+        'W1':np.random.randn(n_h, n_x)* 0.01,
+        'b1':np.zeros((n_h, 1)),
+        'W2':np.random.randn(n_y, n_h)* 0.01,
+        'b2':np.zeros((n_y, 1)),
+    }
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> SIGMOID. Inputs: "X, W1, b1, W2, b2". Output: "A1, cache1, A2, cache2".
+        A1, cache1 = linear_activation_forward(X, W1, b1, "relu")
+        A2, cache2 = linear_activation_forward(A1, W2, b2, "sigmoid")
+        
+        # Compute cost
+        cost = compute_cost(A2, Y)
+        
+        # Initializing backward propagation
+        dA2 = - (np.divide(Y, A2) - np.divide(1 - Y, 1 - A2))
+        
+        # Backward propagation. Inputs: "dA2, cache2, cache1". Outputs: "dA1, dW2, db2; also dA0 (not used), dW1, db1".
+        dA1, dW2, db2 = linear_activation_backward(dA2, cache2, 'sigmoid')
+        dA0, dW1, db1 = linear_activation_backward(dA1, cache1, "relu")
+        
+        # Set grads['dWl'] to dW1, grads['db1'] to db1, grads['dW2'] to dW2, grads['db2'] to db2
+        grads['dW1'] = dW1
+        grads['db1'] = db1
+        grads['dW2'] = dW2
+        grads['db2'] = db2
+        
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        # Retrieve W1, b1, W2, b2 from parameters
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        
+        # Print the cost every 100 iterations and for the last iteration
+        if print_cost and (i % 100 == 0 or i == num_iterations - 1):
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if i % 100 == 0:
+            costs.append(cost)
+            
+    return parameters, costs
+
+def plot_costs(costs, learning_rate=0.0075):
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+# ------------------------------------------------------------------------------------------------------------
+# GRADED FUNCTION: L_layer_model
+
+def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
+
+    np.random.seed(1)
+    costs = []
+    
+    # Parameters initialization.
+    parameters = initialize_parameters_deep(layers_dims)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        AL, caches = L_model_forward(X, parameters)
+        
+        # Compute cost.
+        cost = compute_cost(AL, Y)
+    
+        # Backward propagation.
+        grads = L_model_backward(AL, Y, caches)
+ 
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+                
+        # Print the cost every 100 iterations and for the last iteration
+        if print_cost and (i % 100 == 0 or i == num_iterations - 1):
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if i % 100 == 0:
+            costs.append(cost)
+    
+    return parameters, costs
