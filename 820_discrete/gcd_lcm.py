6241da7ceab7043532fc9a8dd64906051e4820b0
方思渝 <fangsihyu0211@gmail.com>
2025-11-06 13:56:33 +0100

    add: the gcd amd lcm tool

---
+++ 820_discrete/gcd_lcm.py
@@ -0,0 +1,13 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    l = (a*b) // gcd(a, b)
+    return (l)
+
+def package(a, b):
+    print(f"the gcd: {gcd(a, b)}, the lcm: {lcm(a, b)}, the a*b {a*b}")
+
+package(18, 24)
